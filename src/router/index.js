cec4cb582ade6d98cdc9a169687f0018ac679db7
Semyon <saymon221090@gmail.com>
2021-03-15 22:26:29 +0300

    Настраивает редирект. Изменяет рутинг осонвного компонента

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import TrafficLight from '../components/TrafficLight.vue';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/red',
-            name: 'red'
-        }
-    ]
-})
+  mode: 'history',
+  routes: [
+    { path: '/', redirect: '/red' },
+    {
+      path: '/:color',
+      name: 'traffic-light',
+      component: TrafficLight,
+    },
+  ],
+});
