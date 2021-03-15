c072b00f4fae0e4c12e03329a95c176eb670648b
Semyon <saymon221090@gmail.com>
2021-03-15 22:22:26 +0300

    корректирует настройки сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
 const path = require(`path`);
-const {VueLoaderPlugin} = require('vue-loader');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-    mode: "development",
-    entry: `./src/index.js`,
-    devtool: `source-map`,
-    output: {
-        filename: `bundle.js`,
-        path: path.join(__dirname, `public`)
-    },
-    devServer: {
-        contentBase: path.join(__dirname, `public`),
-        inline: false,
-        port: 1337,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ],
-    },
-    plugins: [
-        new VueLoaderPlugin()
-    ]
+  mode: 'development',
+  entry: `./src/index.js`,
+  devtool: `source-map`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    inline: false,
+    port: 1337,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
 };
