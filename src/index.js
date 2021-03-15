37afcbfc09f8e1387c74f3d59140aadcbc3c6f7d
Semyon <saymon221090@gmail.com>
2021-03-15 22:29:32 +0300

    встраивает store в приложение

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import router from './router';
-import App from "./App.vue";
+import App from './App.vue';
+import store from './store';
+import './style.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App),
+  store,
+  render: h => h(App),
 }).$mount('#App');
