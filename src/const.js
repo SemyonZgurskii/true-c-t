f0cbbc60ee5c5f0011e2e39610ede112be70a665
Semyon <saymon221090@gmail.com>
2021-03-15 22:28:12 +0300

    создает словари "роут-сигнал" и обратно

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const RouteToSignal = {
+  '/red': 'stop',
+  '/yellow': 'attention',
+  '/green': 'go',
+};
+
+export const SignalToRoute = {
+  stop: 'red',
+  attention: 'yellow',
+  go: 'green',
+};
